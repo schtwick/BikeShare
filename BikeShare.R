06d197c9436c6fdbd9db4af32323d790d7b095c4
Stockton Aubrey <stocktonaubrey@Stocktons-MacBook-Air.local>
2024-09-16 09:19:19 -0600

    Added regression models

--- BikeShare.R
+++ BikeShare.R
@@ -4,6 +4,7 @@ library(patchwork)
 library(vroom)
 
 data <- vroom('train.csv')
+testData <- vroom('test.csv')
 glimpse(data)
 
 data <- data %>%
@@ -31,3 +32,51 @@ p4 <- ggplot(data, aes(x = weekday, y = count)) +
 
 combined_plot <- p1 + p2 + p3 + p4 + plot_layout(ncol = 2)
 combined_plot
+
+library(tidymodels)
+
+## Setup and Fit the Linear Regression Model
+my_linear_model <- linear_reg() %>% # Type of model
+  set_engine("lm") %>% # Engine = What R function to use
+  set_mode("regression") %>% # Regression just means quantitative response
+  fit(formula = log(count) ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed, data = data)
+
+bike_predictions <- predict(my_linear_model, new_data = testData) 
+
+
+
+
+library(vroom)
+
+kaggle_submission <- bike_predictions %>%
+  bind_cols(., testData) %>%
+  select(datetime, .pred) %>%
+  rename(count = .pred) %>%
+  mutate(count = pmax(0, count)) %>%
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x = kaggle_submission, file = "./LinearPreds.csv", delim = ",")
+
+
+library(poissonreg)
+
+my_pois_model <- poisson_reg() %>%
+  set_engine("glm") %>%
+  set_mode("regression") %>%
+  fit(formula = count ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed, data = data)
+
+bike_predictions2 <- predict(my_pois_model, new_data = testData)
+bike_predictions2
+
+
+pois_kaggle_submission <- bike_predictions2 %>%
+  bind_cols(., testData) %>%
+  select(datetime, .pred) %>%
+  rename(count = .pred) %>%
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x = pois_kaggle_submission, file = "./PoissonPreds.csv", delim = ",")
+
+
+
+
